b25c638f1741f30071c0fd1e26e65422412b9b68
full-deev <sebassando557@gmail.com>
2024-05-25 11:54:57 -0500

    TareaTienda casi completo, falta la parte de VENDER

--- prueba.py
+++ prueba.py
@@ -12,7 +12,7 @@ while opcion != 0:
                 "   |                           |\n"
                 "   |        LA TIENDA          |\n"
                 "   |                           |\n"
-                "   |            UTE ☻          |\n"
+                "   |            ABC ♦          |\n"
                 "   +---------------------------+\n")
     print(            
         "    0 -> Salir\n"
@@ -66,8 +66,9 @@ while opcion != 0:
         if ide < 0:
             ide = int(input("Revisa ID e ingresa nuevamente: "))
         for i in range(len(productos)):           
-            if ide != productos[i]['id']:
+            if ide != productos[i]['id']:                
                 print("El ID no tiene registros asociados.")
+                continue
             else:
                 print("Datos Encontrados\n", 
                       "\nProducto: ", productos[i]['nombre']
@@ -75,23 +76,62 @@ while opcion != 0:
                       ,"\nStock: ", productos[i]['stock']) 
                 break       
         print(f"\n consultar{productos}")
-
         '''Estoy modificando los productos'''
-    elif opcion == 4:
+    elif opcion == 4:        
         print("\n****Menu Modificar Stock**** \n")
-
+        ide = int(input("Ingrese el ID a consultar "))
+        if ide < 0:
+            ide = int(input("Revisa ID e ingresa nuevamente: "))     
+        for i in range(len(productos)):
+            if ide == productos[i]['id']:
+                print("Stock actual ", productos[i]['stock'])
+                nombre = productos[i]['nombre']
+                precio = productos[i]['precio']
+                break
+        stock = int(input("Ingrese el nuevo stock: "))
+        if stock < 0:
+            stock = int(input("Stock no válido, ingrese nuevo: "))                     
+        productos.append({'id':ide,'nombre':nombre,
+                        'precio':precio,'stock':stock})            
         print(f"\n modificar  {productos}")
 
         '''Estoy vendiendo productos'''
     elif opcion == 5:
-        print("\n****Menu Vender**** \n")
+        print("\n****Menu Vender****")
+        for i in range(len(productos)):
+            print(f"\n ID-",productos[i]['id'], productos[i]['nombre'],"en stock:",productos[i]['stock'])
+        ide = int(input("Ingrese el ID a vender: "))
+        if ide < 0:
+            ide = int(input("Revisa ID e ingresa nuevamente: "))        
+        for j in range(len(productos)):
+            if ide == productos[j]['id']:
+                buy = int(input("Ingresa la cantidad a vender: "))
+                if buy < 0:
+                    buy = int(input("Cantidad ingresada no valida, ingrese otra: "))
+                    if buy > productos[j]['stock']:
+                        print("Cantidad a comprar no puede ser mayor a la mostrada en pantalla")
+                    else:
+                        res = ((productos[j][stock]) - (buy))     
+                        print(res)  
+            else:
+                continue
+                print(f"{ide}, no encontrado")
 
         '''Vamos a mostrar inventario'''
     elif opcion == 6:
         print("\n****Menu Inventario**** \n")
         print("TIENDA ABC ♣")
-        print(productos['id'],productos['nombre'])
+        print("INVENTARIO TIENDA ABC ♦\n")
+        print("ID","NOM","CASH","CANT", sep=" | ", end=" |\n")
+        for i in range(len(productos)):
+            idd = productos[i]['id']
+            name = productos[i]['nombre']
+            price = productos[i]['precio']
+            size = productos[i]['stock']            
+            print(idd, name, price, size, sep=" | ", end=" | \n")
+            #print("ID {} - PRODUCTO {} PRECIO {} CANTIDAD {}".format(idd, name, price, size))
+
+        #print (productos)
     else:
         if opcion == 0:print("\nHasta luego, vuelva pronto... ♥")
         else: print("Digitos, no válidos, corrijalos !!")
-    

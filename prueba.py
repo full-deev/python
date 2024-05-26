b5b6b8c3a63490685b6487b1e0d5dad739e6cdee
full-deev <sebassando557@gmail.com>
2024-05-26 03:26:20 -0500

    He, acabado, lo unico que me falta es que el print dentro del bucle no se imprima las veces del bucle, ya estaaa ☺

--- prueba.py
+++ prueba.py
@@ -1,10 +1,11 @@
 opcion = " "
+'''Datos de prueba para agilizar un poco el proceso'''
 productos =[{'id': 101, 'nombre': 'ppas', 'precio': 10, 'stock': 15}, 
           {'id': 102, 'nombre': 'papas', 'precio': 20, 'stock': 26}, 
           {'id': 103, 'nombre': 'arroz', 'precio': 65, 'stock': 99}, 
           {'id': 104, 'nombre': 'frutas', 'precio': 25, 'stock': 50},
           {'id': 105, 'nombre': 'harina', 'precio': 15, 'stock': 50},
-          {'id': 106, 'nombre': 'gelatinas', 'precio': 88, 'stock': 13}]
+          {'id': 106, 'nombre': 'huevos', 'precio': 88, 'stock': 13}]
 while opcion != 0:
     print("")
     print(      "   +---------------------------+\n"
@@ -24,7 +25,6 @@ while opcion != 0:
         "    6 -> Mostrar inventario\n"
         )
     opcion = int((input("Ingresa el numero de la opcion a consultar: ")))
-    print(productos)
     '''Estoy agregando productos'''
     if opcion == 1:
         print("\n****Menu Agregar Productos****\n")
@@ -33,7 +33,7 @@ while opcion != 0:
                 ide = int(input("Revisa ID e ingresa nuevamente: "))
         for i in range(len(productos)):
             if ide == productos[i]['id']:
-                print(("Ya existe, vamos actualizarlo..."))
+                print(("Ya existe, es necesario una actualización..."))
                 productos.pop(i)
                 break
         nombre = input("Ingrese el nombre del producto: ")
@@ -45,7 +45,6 @@ while opcion != 0:
             stock = int(input("Stock no válidp, ingrese nuevo: "))        
         productos.append({'id':ide,'nombre':nombre,
                         'precio':precio,'stock':stock})
-        print(f"agregar  {productos}\n")
         '''Estoy eliminando productos'''
     elif opcion == 2:
         print("\n****Menu Eliminar Productos**** \n")
@@ -58,7 +57,6 @@ while opcion != 0:
                 break
             else:
                 print("Producto no encontrado, agreguelo para así poder eliminarlo.")
-        print(f"\n eliminar y actualizadoo \n  {productos}")
         '''Estoy buscando productos'''
     elif opcion == 3:
         print("\n****Menu Consultar Productos**** \n")
@@ -67,7 +65,7 @@ while opcion != 0:
             ide = int(input("Revisa ID e ingresa nuevamente: "))
         for i in range(len(productos)):           
             if ide != productos[i]['id']:                
-                print("El ID no tiene registros asociados.")
+                #print("El ID no tiene registros asociados.")
                 continue
             else:
                 print("Datos Encontrados\n", 
@@ -75,52 +73,56 @@ while opcion != 0:
                       ,"\nPrecio: ",productos[i]['precio']
                       ,"\nStock: ", productos[i]['stock']) 
                 break       
-        print(f"\n consultar{productos}")
         '''Estoy modificando los productos'''
     elif opcion == 4:        
         print("\n****Menu Modificar Stock**** \n")
         ide = int(input("Ingrese el ID a consultar "))
         if ide < 0:
-            ide = int(input("Revisa ID e ingresa nuevamente: "))     
+            ide = int(input("Revisa ID e ingresa nuevamente: "))
         for i in range(len(productos)):
             if ide == productos[i]['id']:
-                print("Stock actual ", productos[i]['stock'])
-                nombre = productos[i]['nombre']
-                precio = productos[i]['precio']
-                break
-        stock = int(input("Ingrese el nuevo stock: "))
-        if stock < 0:
-            stock = int(input("Stock no válido, ingrese nuevo: "))                     
-        productos.append({'id':ide,'nombre':nombre,
-                        'precio':precio,'stock':stock})            
-        print(f"\n modificar  {productos}")
-
-        '''Estoy vendiendo productos'''
+                print("Stock actual", productos[i]['stock'])
+                stock = int(input("Ingrese el nuevo stock: "))
+                if stock < 0:
+                    stock = int(input("Stock no válido, ingrese nuevo: "))
+                    nombre = productos[i]['nombre']
+                    precio = productos[i]['precio']              
+                    productos.pop(i)                                 
+                    productos.append({'id':ide,'nombre':nombre,'precio':precio,'stock':stock})
+                    break
+                else:
+                    nombre = productos[i]['nombre']
+                    precio = productos[i]['precio']              
+                    productos.pop(i)                                 
+                    productos.append({'id':ide,'nombre':nombre,'precio':precio,'stock':stock})
+                    break
     elif opcion == 5:
-        print("\n****Menu Vender****")
+        print("****Articulos Disponibles****\n")
         for i in range(len(productos)):
-            print(f"\n ID-",productos[i]['id'], productos[i]['nombre'],"en stock:",productos[i]['stock'])
-        ide = int(input("Ingrese el ID a vender: "))
+            print(f"ID-",productos[i]['id'], productos[i]['nombre'],"en stock:",productos[i]['stock'])
+        ide = int(input("\nIngrese el ID a vender: "))
         if ide < 0:
             ide = int(input("Revisa ID e ingresa nuevamente: "))        
-        for j in range(len(productos)):
-            if ide == productos[j]['id']:
-                buy = int(input("Ingresa la cantidad a vender: "))
-                if buy < 0:
-                    buy = int(input("Cantidad ingresada no valida, ingrese otra: "))
-                    if buy > productos[j]['stock']:
-                        print("Cantidad a comprar no puede ser mayor a la mostrada en pantalla")
-                    else:
-                        res = ((productos[j][stock]) - (buy))     
-                        print(res)  
+        for i in range(len(productos)):
+            if ide == productos[i]['id']:
+                cant = int(input("Ingresa la cantidad a vender: "))
+                if cant > 0 and cant < productos[i]['stock']:
+                    nombre = productos[i]['nombre']
+                    precio = productos[i]['precio']
+                    stock = productos[i]['stock']
+                    stock = stock - cant
+                    tventa = precio * cant
+                    productos.pop(i)
+                    print("Venta en proceso !! ♦\n")
+                    print(f"Su compra de {nombre} con numero de unidades: {cant} es un total de $ {tventa} \n")
+                    productos.append({'id': ide,'nombre':nombre,'precio':precio,'stock':stock})
+                else:
+                    print("Revise los datos ingresados y vuelva a ingresar.")                    
             else:
                 continue
-                print(f"{ide}, no encontrado")
-
         '''Vamos a mostrar inventario'''
     elif opcion == 6:
         print("\n****Menu Inventario**** \n")
-        print("TIENDA ABC ♣")
         print("INVENTARIO TIENDA ABC ♦\n")
         print("ID","NOM","CASH","CANT", sep=" | ", end=" |\n")
         for i in range(len(productos)):
@@ -129,9 +131,6 @@ while opcion != 0:
             price = productos[i]['precio']
             size = productos[i]['stock']            
             print(idd, name, price, size, sep=" | ", end=" | \n")
-            #print("ID {} - PRODUCTO {} PRECIO {} CANTIDAD {}".format(idd, name, price, size))
-
-        #print (productos)
     else:
         if opcion == 0:print("\nHasta luego, vuelva pronto... ♥")
         else: print("Digitos, no válidos, corrijalos !!")
